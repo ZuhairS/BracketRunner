1225e3a85d63aedf17a1b1a1cfd815ab44dd2d17
Nick <newhitson@gmail.com>
2017-08-08 10:08:30 -0700

    add alerts_reducer

--- app/reducers/root_reducer.js
+++ app/reducers/root_reducer.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 
 import bracketReducer from './bracket_reducer';
 import AuthReducer from ./'auth_reducer';
+import AlertsReducer from './alerts_reducer';
 
 module.exports = combineReducers({
   bracket: bracketReducer,
-  auth: AuthReducer
+  auth: AuthReducer,
+  alerts: AlertsReducer
 });
