492547772dea3586cbc8c1802821ad578a8d21c7
Nick <newhitson@gmail.com>
2017-08-08 17:48:55 -0700

    attempt to merge auth forms

--- app/components/auth.js
+++ app/components/auth.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-  ScrollView
+  ScrollView,
+  TextInput,
 } from 'react-native';
+
+import MyTextInput from './MyTextInput';
 
 import Home from './home';
 
-var Bracket = React.createClass({
+var Auth = React.createClass({
   navigateToHome: function() {
     this.props.navigator.pop({
       component: Home,
@@ -18,36 +22,58 @@ var Bracket = React.createClass({
       navigationBarHidden: true
     });
   },
+  signUp: function(values){
+    console.log('submitting form', values)
+    console.log(this.props);
+  },
+  logIn: function(values){
+    console.log('submitting form', values)
+  },
   render() {
-    // const {Auth} = this.props;
-
+    console.log(this.props);
     return (
-      <View style={bracketStyles.container}>
-      <TouchableOpacity onPress={this.navigateToHome}>
-        <Text>Home</Text>
+      <View style={styles.container}>
+      <Text>Email:</Text>
+      <Field name="email" component={renderInput} />
+      <Text>Password:</Text>
+      <Field name="password" component={renderInput} />
+      <TouchableOpacity onPress={this.props.handleSubmit(this.signUp)}>
+        <Text style={styles.button}>Sign Up</Text>
       </TouchableOpacity>
-      </View>
+      <TouchableOpacity onPress={this.props.handleSubmit(this.logIn)}>
+        <Text style={styles.button}>Log In</Text>
+      </TouchableOpacity>
+    </View>
     );
   }
 });
 
-//&#160;; (space without line break)
 
-const bracketStyles = StyleSheet.create({
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'blue',
+    color: 'white',
+    height: 30,
+    lineHeight: 30,
+    marginTop: 10,
+    textAlign: 'center',
+    width: 250
+  },
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-    width: 420,
-    paddingTop: 25
-  }
-});
 
-var mapStatetoProps = ({bracket}) => {
-  return {
-    matches: bracket.matches
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    height: 37,
+    width: 250
   }
+})
+
+const renderInput = ({ input: { onChange, ...restInput }}) => {
+  return <TextInput style={styles.input} onChangeText={onChange} {...restInput} />
 }
 
-module.exports = connect(mapStatetoProps)(Bracket);
+export default reduxForm({
+  form: 'test'
+},null, mapDispatchToProps)(Auth)
