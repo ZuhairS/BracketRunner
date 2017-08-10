6e92dc32041f1f3702898880878ce6928bf96be2
Nick <newhitson@gmail.com>
2017-08-10 14:44:03 -0700

    test

--- app/reducers/root_reducer.js
+++ app/reducers/root_reducer.js
@@ -3,6 +3,7 @@ import {combineReducers} from 'redux';
 import AuthReducer from './auth_reducer';
 import AlertsReducer from './alerts_reducer';
 import bracketReducer from './bracket_reducer';
+import { reducer as formReducer } from 'redux-form';
 
 module.exports = combineReducers({
   auth: AuthReducer,
