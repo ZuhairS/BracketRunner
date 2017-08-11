73994eb8dde53204f51f286f750e75ab3cdfc6ff
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-08-11 12:52:07 -0700

    Create user reducer and being work on user actions

--- app/actions/user_actions.js
+++ app/actions/user_actions.js
@@ -1,37 +1,21 @@
 import axios from 'axios';
 
-import { GET_URL, EDIT_URL } from '../util/auth_api_util';
+import { GET_URL, EDIT_URL } from '../util/user_api_util';
+
+import { addAlert } from './alerts_actions';
 
 export const RECEIVE_USER = 'RECEIVE_USER';
-export const UPDATE_USER = 'UPDATE_USER';
 
-exports.editUser = ({
-  picture,
-  twitter,
-  twitch,
-  youTube,
-  sponsor,
-  sponsor_picture,
-  about
-}) => {
+exports.editUser = userProps => {
   return function(dispatch) {
     return axios
-      .patch(EDIT_URL, {
-        picture,
-        twitter,
-        twitch,
-        youTube,
-        sponsor,
-        sponsor_picture,
-        about
-      })
+      .patch(EDIT_URL, userProps)
       .then(response => {
-        var { user_id, token } = response.data;
-        dispatch(addAlert(token));
-        dispatch(editUser(user_id));
+        var { user } = response.data;
+        dispatch(receiveUser(user));
       })
       .catch(error => {
-        dispatch(addAlert('Could update profile'));
+        dispatch(addAlert('Could not update profile'));
       });
   };
 };
