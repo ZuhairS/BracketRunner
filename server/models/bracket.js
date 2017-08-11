d74eab3204f621b0776b3fb69a243fab63eb7dbb
Ali Haq <ali.m.haq@gmail.com>
2017-08-11 10:36:42 -0700

    Add to bracket model

--- server/models/bracket.js
+++ server/models/bracket.js
@@ -2,6 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let bracketSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Need a name!']
+  },
+  numberOfEntrants: {
+    type: Number,
+    enum: [8, 16, 32, 64],
+    required: true
+  },
+  entrants: {
+    type: [],
+    required: true
+  },
+  game: {
+    type: String,
+    required: [true, 'Need to choose a game!']
+  },
+  startTime: {
+    type: Date,
+    required: true
+  },
+  endTime: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: String,
+    required: [true, 'Need a location!']
+  },
+  live: {
+    type: Boolean,
+    // required: true
+  },
+  // matches or rounds with match objects inside?
   matches: [
     {
       player1: {
