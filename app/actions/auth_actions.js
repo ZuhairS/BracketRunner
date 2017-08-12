ab6f861035ced6965f0ef5abb289eb4a239edfbd
Nick <newhitson@gmail.com>
2017-08-11 19:56:36 -0700

    looks good

--- app/actions/auth_actions.js
+++ app/actions/auth_actions.js
@@ -3,26 +3,12 @@ import axios from 'axios';
 import {SIGNIN_URL, SIGNUP_URL, SIGNOUT_URL} from '../util/auth_api_util';
 // import * as APIUtil from '../util/auth_api_util';
 // if we get this to work lets refactor it will this better style
-import {addAlert} from './alerts_actions';
+import { addAlert } from './alerts_actions';
 
 export const AUTH_USER = 'AUTH_USER';
 export const UNAUTH_USER = 'UNAUTH_USER';
 
-
-exports.logInUser = ({email, username, password}) => {
-  return function(dispatch) {
-    return axios.post(SIGNIN_URL, {email, username, password}).then((response) => {
-      var {user_id, token} = response.data;
-      dispatch(addAlert(token));
-      dispatch(authUser(user_id));
-    }).catch((error) => {
-      dispatch(addAlert("Could not sign in"));
-    });
-  }
-}
-
-
-exports.signUpUser = ({email, username, password}) => {
+exports.logInUser = ({ email, username, password }) => {
   return function(dispatch) {
     return axios.post(SIGNUP_URL, {email, username, password}).then((response) => {
       var {user_id, token} = response.data;
@@ -50,9 +36,9 @@ exports.signOutUser = ({user_id}) => {
 authUser = (user_id) => {
   return {
     type: AUTH_USER,
-    user_id
-  }
-}
+    userId
+  };
+};
 
 unauthUser = () => {
   return {
--- app/reducers/root_reducer.js
+++ app/reducers/root_reducer.js
@@ -1,13 +1,15 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 
 import AuthReducer from './auth_reducer';
 import AlertsReducer from './alerts_reducer';
 import bracketReducer from './bracket_reducer';
+import UserReducer from './user_reducer';
 import { reducer as formReducer } from 'redux-form';
 
 module.exports = combineReducers({
   auth: AuthReducer,
   alerts: AlertsReducer,
+  bracket: bracketReducer,
   form: formReducer,
-  bracket: bracketReducer
+  users: UserReducer
 });
