e7d980eea6b0f8cb684acf9bd810a1aa7dac5c5d
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-08-11 20:28:21 -0700

    Add bracket actions and reducers

--- app/reducers/bracket_reducer.js
+++ app/reducers/bracket_reducer.js
@@ -1,29 +1,25 @@
+import {
+  RECEIVE_USER,
+  RECEIVE_ALL_BRACKETS,
+  RECEIVE_LIVE_BRACKETS
+} from '../actions/bracket_actions';
+
 var defaultState = {
-  matches: [
-    {
-      player1: 'nick',
-      player2: 'zuhair'
-    },
-    {
-      player1: 'zach',
-      player2: 'ali'
-    },
-    {
-      player1: 'younis',
-      player2: 'chris'
-    },
-    {
-      player1: 'josh',
-      player2: 'andrew'
-    }
-  ]
+  selectedBracket: {},
+  allBrackets: {},
+  liveBrackets: {}
 };
 
 module.exports = (state = defaultState, action) => {
   switch (action.type) {
-    case 'RECEIVE_BRACKET':
-      return defaultState;
-    // return Object.assign({}, state, {bracket: action.bracket});
+    case RECEIVE_BRACKET:
+      return Object.assign({}, state, { selectedBracket: action.bracket });
+
+    case RECEIVE_ALL_BRACKETS:
+      return Object.assign({}, state, { allBrackets: action.brackets });
+
+    case RECEIVE_LIVE_BRACKETS:
+      return Object.assign({}, state, { liveBrackets: action.brackets });
 
     default:
       return state;
--- app/util/bracket_api_util.js
+++ app/util/bracket_api_util.js
@@ -1,4 +1,5 @@
 const API_URL = 'http://localhost:3000/api/v1/';
 
-export const BRACKET_URL = (bracket_id) => `${API_URL}/brackets/${bracket_id}`;
-export const BRACKETS_URL = () => `${API_URL}/brackets`;
+export const BRACKET_URL = bracketId => `${API_URL}/brackets/${bracketId}`;
+
+export const BRACKETS_URL = `${API_URL}/brackets`;
