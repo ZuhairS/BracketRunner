c68a672d3e2424fde0b39691618dcb523e252ea4
Ali Haq <ali.m.haq@gmail.com>
2017-08-11 21:15:32 -0700

    Add show and edit bracket controller methods

--- server/controllers/brackets_controller.js
+++ server/controllers/brackets_controller.js
@@ -4,15 +4,19 @@ const Bracket = require('../models/bracket');
 exports.create = function(req, res, next) {
   var bracket = new Bracket({
     name: req.body.name,
+    numberOfEntrants: req.body.numberOfEntrants,
     entrants: req.body.entrants,
-    game: req.body.game
-    // etc. need to clarify with zach
+    game: req.body.game,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+    location: req.body.location,
+    rounds: req.body.rounds
   });
   bracket.save(function(error) {
     if (error) {
       return next(error);
     }
-    res.json({ bracket });
+    res.json(bracket); // Do we want to return the bracket once it's created?
   });
 };
 
@@ -21,20 +25,52 @@ exports.create = function(req, res, next) {
 //   res.json({});
 // };
 
-// Display list of Brackets through GET
-exports.bracketList = function(req, res) {
-    res.send('TODO: bracket list');
-};
+// Display list of Brackets through GET; for the bracket feed
+// exports.bracketList = function(req, res) {
+//     res.send('TODO: bracket list');
+// };
 
 // Display specific Bracket through GET
-exports.show = function(req, res) {
-    res.send('TODO: bracket detail: ' + req.params.id);
+exports.show = function (req, res, next) {
+  Bracket.findById(req.params.id).exec((err, bracket) => {
+      if (err) {
+        res.send({ error: err });
+        return next(err);
+      }
+      res.json(bracket);
+    });
 };
 
 // Update Bracket through PUT
-exports.edit = function(req, res) {
-    res.send('TODO: bracket update ');
+exports.edit = function(req, res, next) {
+  Bracket.findByIdAndUpdate(req.params.id, { $set: 
+    {
+    name: req.body.name,
+    numberOfEntrants: req.body.numberOfEntrants,
+    entrants: req.body.entrants,
+    game: req.body.game,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+    location: req.body.location,
+    rounds: req.body.rounds
+    }
+  }).exec(function(err, bracket){
+      if (err) {
+        res.send({ error: err });
+        return next(err);
+      }
+      res.json(bracket);
+      });
 };
+
+Book.findOneAndUpdate({ "_id": bookId }, { "$set": { "name": name, "genre": genre, "author": author, "similar": similar}}).exec(function(err, book){
+  if(err) {
+      console.log(err);
+      res.status(500).send(err);
+  } else {
+           res.status(200).send(book);
+  }
+});
 
 // Delete a bracket through DELETE
 exports.delete = function(req, res) {
