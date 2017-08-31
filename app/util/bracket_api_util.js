fc18e87c7a6cb7f83dd09748be085592b2c6f422
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-08-31 14:23:49 -0700

    Add production api links, allow sign up again

--- app/util/bracket_api_util.js
+++ app/util/bracket_api_util.js
@@ -1,5 +1,5 @@
-// const API_URL = 'https://bracket-runner.herokuapp.com/api/v1';
-const API_URL = 'http://localhost:3000/api/v1';
+const API_URL = 'https://bracket-runner.herokuapp.com/api/v1';
+// const API_URL = 'http://localhost:3000/api/v1';
 
 export const BRACKET_URL = bracketId => `${API_URL}/brackets/${bracketId}`;
 
